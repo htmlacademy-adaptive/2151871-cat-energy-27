3726a09d9b46ddbdbaaec00a983248de06198e94
Arcanumsbc <arcanumsbc@yandex.ru>
2023-01-16 19:30:54 +0300

    Адаптивная графика правка3

--- source/js/script.js
+++ source/js/script.js
@@ -13,29 +13,29 @@ navToggle.addEventListener('click', function () {
   }
 });
 
-const slider = document.querySelector(".cat-comparison")
-const curtain = slider.querySelector(".range-bar")
-let sliderStyles = getComputedStyle(slider)
-let curtainPlaceStart
-let clientX
+const slider = document.querySelector(".cat-comparison");
+const curtain = slider.querySelector(".range-bar");
+let sliderStyles = getComputedStyle(slider);
+let curtainPlaceStart;
+let clientX;
 
 function handleGrabCurtain (event) {
-	curtainPlaceStart = +(sliderStyles.getPropertyValue("--curtain-place"))
-  clientX = event.clientX
-	window.addEventListener("pointermove", handleMoveCurtain)
+	curtainPlaceStart = +(sliderStyles.getPropertyValue("--curtain-place"));
+  clientX = event.clientX;
+	window.addEventListener("pointermove", handleMoveCurtain);
 }
 
 function handleMoveCurtain (event) {
-	let deltaX = event.clientX - clientX
-	let cursorPlace = curtainPlaceStart + deltaX / slider.clientWidth
-	let curtainPlace = Math.min(Math.max(cursorPlace, 0), 1)
-	slider.style.setProperty("--curtain-place", `${curtainPlace}`)
+	let deltaX = event.clientX - clientX;
+	let cursorPlace = curtainPlaceStart + deltaX / slider.clientWidth;
+	let curtainPlace = Math.min(Math.max(cursorPlace, 0), 1);
+	slider.style.setProperty("--curtain-place", `${curtainPlace}`);
 }
 
 function handleReleaseCurtaine (event) {
-	window.removeEventListener("pointermove", handleMoveCurtain)
+	window.removeEventListener("pointermove", handleMoveCurtain);
 }
 
-window.addEventListener("pointerup", handleReleaseCurtaine)
+window.addEventListener("pointerup", handleReleaseCurtaine);
 
-curtain.addEventListener("pointerdown", handleGrabCurtain)
+curtain.addEventListener("pointerdown", handleGrabCurtain);
